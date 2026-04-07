c778ca7f41b02f408bb878f083e3daaf029d5f05
Brian Kenkel <briankenkel.t@gmail.com>
2026-04-07 10:13:28 -0400

    update strat

--- lint.sh
+++ lint.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+skip_dirs=()
+while IFS= read -r -d '' marker; do
+  skip_dirs+=("-not" "-path" "$(dirname "$marker")/*")
+done < <(find . -name '.skiplint' -print0)
+
 files=()
 while IFS= read -r -d '' f; do
   files+=("$f")
-done < <(find . \( -name '*.yml' -o -name '*.yaml' \) -print0 | sort -z)
+done < <(find . \( -name '*.yml' -o -name '*.yaml' \) "${skip_dirs[@]}" -print0 | sort -z)
 
 if [ ${#files[@]} -gt 0 ]; then
   dclint --fix "${files[@]}"
---
+++ stack/setup.md
@@ -0,0 +1,19 @@
+# Global Search and Replace Terms
+
+## Name of The Stack
+
+```plain
+replaceme.STACKNAME
+```
+
+### Name of The App/Service
+
+```plain
+replaceme.APPNAME
+```
+
+### Version Tag
+
+```plain
+replaceme.VERSTAG
+```
